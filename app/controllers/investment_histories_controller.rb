4c58f47908c3e074dc0a1f02afc2724af5ea3b7c
Masaki Nishitsuji <masaki.nishitsuji@housmart.com>
2018-11-22 05:55:23 +0900

    fixed investment_history search scope

--- app/controllers/investment_histories_controller.rb
+++ app/controllers/investment_histories_controller.rb
@@ -1,16 +1,13 @@
 class InvestmentHistoriesController < ApplicationController
-    def index
-        binding.pry
-        @investment_histories = InvestmentHistory.search_by_investment_name(params[:investment])
-            .search_by_update_year(params[:year].to_i)
-            .search_by_update_month(params[:month].to_i)
+  def index
+    @investment_histories = InvestmentHistory.search(params[:investment], params[:year], params[:month])
 
-        #/views/investment_histories/index.html.erb内の検索値に初期値を設定する。
-        #@investment_number = Investment.all.find_index(Investment.find_by(id: investment_params[:id].to_i)) + 1
-        @update_year = params[:year]
-        @update_month = params[:month]
-    end
-    
-    def show
-    end
+    #/views/investment_histories/index.html.erb内の検索値に初期値を設定する。
+    #@investment_number = Investment.all.find_index(Investment.find_by(id: investment_params[:id].to_i)) + 1
+    @update_year = params[:year]
+    @update_month = params[:month]
+  end
+
+  def show
+  end
 end
